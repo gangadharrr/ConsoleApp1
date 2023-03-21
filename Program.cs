a5ac423659f9b4dd7721d67a7171ecd293247b9e
Star <gangadharganga90@gmail.com>
2023-03-21 11:39:11 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void modify(int vl)
+        {
+            vl++;
+            Console.WriteLine(vl);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age == 5)
+            {
+                goto after;
+            }
+            Console.WriteLine("wlcome");
+            return;
+        after:
+            Console.WriteLine("not");
+            Console.WriteLine(age);
+            modify(age);
+            Console.WriteLine(age);
+
+
+        }    
+    }
+}
