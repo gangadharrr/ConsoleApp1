7d98d3cbf4abf06b6b18e73883f3fc5337e42c6c
Gangadhar C <gangadharganga90@gmail.com>
2023-03-24 15:48:14 +0530

    exchange first and last char in str

--- Program.cs
+++ Program.cs
@@ -5,48 +5,27 @@ using System.Collections;
 using System.ComponentModel;
 using System.Security.Cryptography;
 
+/*Write a C# Sharp program to exchange the first and last characters 
+    in a given string and return the new string*/
 namespace ConsoleApp1
 {
     internal class Program
     {
        
        public static void Main(string[] args)
-        { 
-            Dictionary <int,string> dict = new Dictionary<int,string>();
-            dict.Add(1, "a");
-            dict.Add(2, "b");
-            dict.Add(3, "c");
-            dict.Add(4, "d");
-            dict.Add(5, "e");
-            dict.Add(6, "f");
-            dict.Add(7, "x");
-            Dictionary <int,int> dict1 = new Dictionary<int,int>();
+        {
+            Console.Write("Enter the string: ");
+            string str=Console.ReadLine();
+            char[] ch = str.ToCharArray();
+            char temp = ch[ch.Length-1];
+            ch[str.Length - 1] = ch[0];
+            ch[0] = temp;
+            Convert.ToString(ch);
+            Console.WriteLine(ch);
 
-            Console.WriteLine(dict1.GetValueOrDefault(8));
-            foreach(KeyValuePair<int,string> kvp in dict) 
-            {
-                if(kvp.Value == "a")
-                {
-                    dict[kvp.Key] = "A";
-                }
-            }
-            foreach(var item in dict.OrderBy(x=>x.Value))
-            {
-                Console.WriteLine(item);
-            }
 
-            foreach(var item in dict)
-            {
-                if(item.Value == "x") 
-                {
-                    dict.Remove(item.Key);
-                }
-            }
-            foreach(KeyValuePair<int,string> kvp in dict) 
-            {
-                Console.WriteLine($"[{kvp.Key},{kvp.Value}]");
-            }
-  
+
+
         }    
     }
 }
