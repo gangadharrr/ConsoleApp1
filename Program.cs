de1db3f3360deebf70aec65d1120859cc0264af3
Gangadhar C <gangadharganga90@gmail.com>
2023-03-27 11:55:20 +0530

    Aggregator methods

--- Program.cs
+++ Program.cs
@@ -8,42 +8,55 @@ using System.Security.Cryptography;
 
 namespace ConsoleApp1
 {
-
-
+    public class Marks
+    {
+        public float total_marks;
+        public string grade;
+        public int no_subs;
+        public float percentage;
+        public Marks(float total_marks,int no_subs)
+        {
+            this.total_marks = total_marks;
+            this.no_subs = no_subs;
+        }
+        public void percentagecal()
+        {
+            percentage =total_marks/no_subs;
+        }
+    }
+    public class Student
+    {
+        string name;
+        int age;
+        string rollno;
+        int dept_id;
+        Marks M;
+        public Student(string name, int age, string rollno, int dept_id,Marks M)
+        {
+            this.name = name;
+            this.age = age;
+            this.rollno = rollno;
+            this.dept_id = dept_id;
+            this.M = M;
+        }
+        public void Display()
+        {
+            M.percentagecal();
+            Console.WriteLine($"Name: {name},\nAge: {age},\nRollNO: {rollno}\nMarks: {M.total_marks},\nPercentage: {M.percentage}");
+        }
+    }
     internal class Program
     {
-        
-        private void Print(String args)
-        { 
-            Console.WriteLine(args);
-        }
-        private void Print(int[] args)
-        { 
-            Console.WriteLine($"[{string.Join(", ",args)}]");
-        }
-        private void Print(String[] args)
-        {
-            Console.WriteLine($"[{string.Join(",\n", args)}]");
-        }
-        private void Print(string fname,string lname)
-        {
-            Console.WriteLine( $"{{\n\sFirstname: {fname}\n\sLastname: {lname}\n}}");
-        }
-       
+
         static void Main(string[] args)
         {
-            Program ps = new Program();
-
-            int[] arr = {1,2,3,4,5};
-            string[] arr1 = { "monday", "tuesday" ,"wednesday","thursday"};
-
+            Marks m = new Marks(350, 5);
+            
+            Student s = new Student("Gangadhar",20,"19s115762",2002,m);
+            s.Display();
+            
 
            
-
-            ps.Print("Hello");
-            ps.Print(arr);
-            ps.Print(arr1);
-            ps.Print("Gangadhar", "C");
    
             
 
