35c41c63a63b4918e5028acd5aa8454af75fd879
Gangadhar C <gangadharganga90@gmail.com>
2023-03-30 15:11:42 +0530

    Polymorphism practice problem

--- Program.cs
+++ Program.cs
@@ -9,60 +9,77 @@ using System.Xml.Serialization;
 using System.Runtime.CompilerServices;
 using System.Reflection;
 using Microsoft.VisualBasic;
+using System.Data;
+using System.Security.Principal;
 
 namespace ConsoleApp1
 {
-   
+
+    /*Polymorphism
+
+    A friend of yours wants to create a special calculator.
+    He only wants two operations to be handled by that calculator
+    i.e. : addition and subtraction.
+    The calculator can only accept either 2 numbers or 3 numbers.
+
+    There are two functionalities for this calculator -
+    1. Normal -> in this role - the calculator works normally
+     - where upon adding the numbers are added,
+    while upon subtracting the numbers are subtracted.
+
+    2. Opposite -> in this role - the calculator works opposite
+    to the normal role - where upon adding the numbers are subtracted,
+     while upon subtracting the numbers are added.
+    That friend have approached you for the help, in creating
+     such a calculator.*/
+    internal class Calculator
+    {
+        public bool Role = true;
+        public Calculator()
+        { }
+        public Calculator(bool Role)
+        {
+            this.Role = Role;
+        }
+        public int Addition(int a, int b)
+        {
+            return Role ? a + b : a - b;
+        }
+        public int Addition(int a, int b, int c)
+        {
+            return Role ? a + b + c : a - b - c;
+        }
+        public int Subtraction(int a, int b)
+        {
+            return Role ? a - b : a + b;
+        }
+        public int Subtraction(int a, int b, int c)
+        {
+            return Role ? a - b - c : a + b + c;
+        }
+    }
     internal class Program
     {
-        public static string path = "D:/Async.txt";
-        public static FileStream file = new FileStream(path, FileMode.OpenOrCreate);
-        public static StreamWriter streamWriter = new StreamWriter(file);
-        public static async Task<string> FirstMethod()
+        public static void Main(string[] args)
         {
-            Console.WriteLine("First Method Started");
-            await Task.Delay(3000);
-             //Task.Delay(3000);// throw error with await
-            streamWriter.Close();
-            file.Close();
-            Console.WriteLine("First Method Ending");
-            string msg = "hi this is method 1";
-            return msg;
-        }
-        public static async Task<string> SecondMethod()
-        {
-            Console.WriteLine("Second Method Started");
-            streamWriter.Write("Hello hi xyz");
-            Console.WriteLine("Second Method Ending");
-            string msg = "hi this is method 2";
-            return msg;
-        }
-        public static async Task<string> ThirdMethod()
-        {
-            Console.WriteLine("Third Method File Open and Written some text");
-            string msg = "third method running";
-            return msg;
-        }
-        public static async Task Main(string[] args)
-        {
-            Task SmTask1 = FirstMethod();
-            Task SmTask2=SecondMethod();
-            Task progess=Task.WhenAny(SmTask1, SmTask2);
-            //await progess;
-            while (true) {
-                if (progess.Status == TaskStatus.RanToCompletion)
-                {
-                    Task SmTask3 = ThirdMethod();
-                    break;
-                } 
-                else
-                { 
-                    Console.WriteLine("Not Completd");
-                }
-                Thread.Sleep(2000);
-            }
+            Calculator normalCalculatorObject = new Calculator(true);
+            Calculator oppositeCalculatorObject = new Calculator(false);
+
+            Console.WriteLine("Normal Calculation :-\n");
+
+            Console.WriteLine($"Addition with 2 args: {normalCalculatorObject.Addition(2, 3)}");
+            Console.WriteLine($"Addition with 3 args: {normalCalculatorObject.Addition(1, 2, 3)}");
+
+            Console.WriteLine($"Subtraction with 2 args: {normalCalculatorObject.Subtraction(2, 3)}");
+            Console.WriteLine($"Subtraction with 3 args: {normalCalculatorObject.Subtraction(2, 3, 4)}\n");
             
-            Console.ReadLine();
+            Console.WriteLine("Opposite Calculation :-\n");
+
+            Console.WriteLine($"Addition with 2 args: {oppositeCalculatorObject.Addition(2, 3)}");
+            Console.WriteLine($"Addition with 3 args: {oppositeCalculatorObject.Addition(1, 2, 3)}");
+
+            Console.WriteLine($"Subtraction with 2 args: {oppositeCalculatorObject.Subtraction(2, 3)}");
+            Console.WriteLine($"Subtraction with 3 args: {oppositeCalculatorObject.Subtraction(2, 3, 4)}");
 
         }
     }
