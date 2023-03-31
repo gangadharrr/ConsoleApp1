36e90e38d3606875cfba4c8f118fd7f41ec0ee73
Gangadhar C <gangadharganga90@gmail.com>
2023-03-31 11:50:59 +0530

    PhotoBook Problem

--- Program.cs
+++ Program.cs
@@ -19,125 +19,64 @@ using System.Security.AccessControl;
 
 namespace ConsoleApp1
 {
-    /*  An event is organized and the following has to happen. 
+    /*  Create a C# program to manage a photo book using OOPs.
 
+    To start, create a class called PhotoBook.  It must also have a public method GetNumberPages that will return the number of photo book pages.
 
-      The chief guest needs to be picked up from the airport. 
-      The stage has to be decorated. 
-      The catering team needs to start preparing food
-      The transport team needs to drop the food raw materials to the venue before the cooking needs to start. 
-      The chief guest's speech needs to be reviewed by the PA
-      The venue needs to be checked for security features like bomb threat, etc
-      Prize distribution is set to happen at the end of the event so all the prizes that need to be presented should be brought to the venue. 
-      The chief guest needs to give the speech. 
-      Prizes need to be distributed. 
-      Food has to be provided to all participants. 
+    The default constructor will create an album with 16 pages. There will be an additional constructor, with which we can specify the number of pages we want in the album.
 
-      Please write a C# program to make use of the Async Programming concepts and write the program so that all 
-        of the above happen in a correct order.*/
+    There is also a AddPhotoBook class whose constructor will create an album with 64 pages.
 
+    Finally create a PhotoBookTest class to perform the following actions:
 
+    Create a default photo book and show the number of pages
+    Create a photo book with 32 pages and show the number of pages
+    Create a large photo book and show the number of pages*/
 
-    internal class Program
+    
+    public class PhotoBook
     {
-        public static async void PickUp()
+        private protected int pages;
+        public PhotoBook()
         {
-            Console.WriteLine("***********************************");
-            Console.WriteLine("Cheif Guest has been PickedUp from Airport");
-            await Task.Delay(10000);
-            Console.WriteLine("Cheif Guest has Arrived At event");
-            Console.WriteLine("-----------------------------------");
-            SpeechDelivery();
+            this.pages = 16;
         }
-        public static async void Decoration()
+        public PhotoBook(int pages)
         {
-            Console.WriteLine("***********************************");
-            Console.WriteLine("Decoration has started at event");
-            await Task.Delay(4000);
-            SecurityCheck();
-            Console.WriteLine("Decoration has complete");
-            Console.WriteLine("-----------------------------------");
+            this.pages = pages;
+        }
+        public int GetNumberPages()
+        { 
+            return pages;
+        }
+    }
+    public class AddPhotoBook : PhotoBook
+    {
+        /*public AddPhotoBook() : base(64)*/
+        public AddPhotoBook()
+        {
+            this.pages = 64;
+        }
+    }
+    public class PhotoBookTest
+    {
+        public PhotoBookTest() { 
+            PhotoBook DefaultBook = new PhotoBook();
+            Console.WriteLine($"Default Book Pages: {DefaultBook.GetNumberPages()}");
+            PhotoBook Book_32 = new PhotoBook(32);
+            Console.WriteLine($"Custom Book Pages: {Book_32.GetNumberPages()}");
+            AddPhotoBook Book_64 = new AddPhotoBook();
+            Console.WriteLine($"Large Book Pages: {Book_64.GetNumberPages()}");
 
         }
-        public static async Task<string> FoodTransport()
-        {
-            Console.WriteLine("***********************************");
-            Console.WriteLine("Raw Materials has Picked at Local Store");
-            string str = "Raw Materials Acquried Food Preparation Inprogress";
-            await Task.Delay(7000);
-            Console.WriteLine("Raw Material has Arrived at event");
-            Console.WriteLine("-----------------------------------");
-            return str;
-        }
-        public static async void FoodPreparation()
-        {
-            Console.WriteLine("***********************************");
-            Console.WriteLine("Food Preparation started at event");
-            string str =await FoodTransport();
-            Console.WriteLine(str);
-            await Task.Delay(10000);
-            Console.WriteLine("Food Preparation Completed at event");
-            Console.WriteLine("-----------------------------------");
 
-        }
-        public static async Task<string> SpeechReview() 
-        {
-            Console.WriteLine("***********************************");
-            Console.WriteLine("Speech Review Has Started By PA");
-            await Task.Delay(2000);
-            string str = "Speech has been review";
-            Console.WriteLine("Speech Review Has Completd By PA");
-            Console.WriteLine("-----------------------------------");
-            return str;
-
-        }
-        public static async void SecurityCheck()
-        {
-            Console.WriteLine("***********************************");
-            Console.WriteLine("Security Check Has Started At Event");
-            await Task.Delay(4000);
-            Console.WriteLine("Security Check Has Completed At Event");
-            Console.WriteLine("-----------------------------------");
-
-        }
-        public static async void PrizeDistribution()
-        {
-            Console.WriteLine("***********************************");
-            Console.WriteLine("Prize Distribution Has Started At Event");
-            await Task.Delay(2000);
-            Lunch();
-            await Task.Delay(5000);
-            Console.WriteLine("Prize Distribution Has Completed At Event");
-            Console.WriteLine("-----------------------------------");
-
-        }
-        public static async void SpeechDelivery()
-        {
-            string str = await SpeechReview();
-            Console.WriteLine("***********************************");
-            Console.WriteLine("Speech by Chief Guest Has Started");
-            Console.WriteLine(str);
-            await Task.Delay(5000);
-            Console.WriteLine("Speech by Chief Guest Has Ended");
-            Console.WriteLine("-----------------------------------");
-            PrizeDistribution();
-            
-        }
-        public static async void Lunch() 
-        {
-            Console.WriteLine("***********************************");
-            Console.WriteLine("Lunch Has Started At Event");
-            await Task.Delay(7000);
-            Console.WriteLine("Lunch Distribution Has Completed At Event");
-            Console.WriteLine("-----------------------------------");
-        }
+    }
+    public class Program
+    {
+      
         public static void Main(string[] args)
         {
-            Decoration();
-            PickUp();
-            FoodPreparation();
-            Console.ReadLine();
-
+           PhotoBookTest test= new PhotoBookTest();
         }
     }
 }
