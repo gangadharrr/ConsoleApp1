15e1ee61532a55e3694be305387dab4334c531a7
Gangadhar C <gangadharganga90@gmail.com>
2023-04-03 18:48:42 +0530

    pigLatin translator

--- Program.cs
+++ Program.cs
@@ -16,72 +16,95 @@ using System.Net.Sockets;
 using System.Runtime.ConstrainedExecution;
 using System.Xml.Linq;
 using System.Security.AccessControl;
+using System.Text.RegularExpressions;
 
 namespace ConsoleApp1
 {
-    /*  Working 9 to 5
-    Write a function that calculates overtime and pay associated with overtime.
-
-    Working 9 to 5: regular hours
-    After 5pm is overtime
-    Your function gets an array with 4 values:
-
-    Start of working day, in decimal format, (24-hour day notation)
-    End of working day. (Same format)
-    Hourly rate
-    Overtime multiplier
-    Your function should spit out:
-
-    $ + earned that day (rounded to the nearest hundreth)
-    Examples
-    OverTime([9, 17, 30, 1.5]) ➞ "$240.00"
-
-    OverTime([16, 18, 30, 1.8]) ➞ "$84.00"
-
-    OverTime([13.25, 15, 30, 1.5]) ➞ "$52.50"
-    2nd example explained:
-
-    From 16 to 17 is regular, so 1 * 30 = 30
-    From 17 to 18 is overtime, so 1 * 30 * 1.8 = 54
-    30 + 54 = $84.00*/
-    
-    public struct Details
-    {
-
-        public double StartTime;
-        public double EndTime;
-        public int HourlyRate;
-        public double OvertimeMultiplier;
-    }
     public class Program
     {
-        public double OverTime(Details details)
+        /*    English to Pig Latin Translator
+
+        Pig latin has two very simple rules:
+
+        If a word starts with a consonant move the first letter(s) of the word, till you reach a vowel, to the end of the word and add "ay" to the end.
+        have ➞ avehay
+        cram ➞ amcray
+        take ➞ aketay
+        cat ➞ atcay
+        shrimp ➞ impshray
+        trebuchet ➞ ebuchettray
+        If a word starts with a vowel add "yay" to the end of the word.
+        ate ➞ ateyay
+        apple ➞ appleyay
+        oaken ➞ oakenyay
+        eagle ➞ eagleyay
+        Write two functions to make an English to pig latin translator. The first function TranslateWord(word) takes a single word and returns that word translated into pig latin.The second function TranslateSentence(sentence) takes an English sentence and returns that sentence translated into pig latin.
+
+        Examples
+        TranslateWord("flag") ➞ "agflay"
+
+        TranslateWord("Apple") ➞ "Appleyay"
+
+        TranslateWord("button") ➞ "uttonbay"
+
+        TranslateWord("") ➞ ""
+
+        TranslateSentence("I like to eat honey waffles.") ➞ 
+            "Iyay ikelay otay eatyay oneyhay afflesway."
+        TranslateSentence("Do you think it is going to rain today?") ➞ 
+            "Oday ouyay inkthay ityay isyay oinggay otay ainray odaytay?"
+        Notes
+        Regular expressions will help you not mess up the punctuation in the sentence.
+        If the original word or sentence starts with a capital letter, the translation should preserve 
+            its case (see examples #2, #5 and #6).
+        */
+        
+        public static string TranslateWord(string word)
         {
-            double NormalPay=0.0,AdditionalPay=0.0;
-            if(details.EndTime > 17)
+            string vowels = "aeiouAEIOU";
+            Regex re = new Regex("[A-Za-z]+");
+
+            if (!(vowels.Contains(word[0])) && re.IsMatch(word))
             {
-                double NormalDifferenceHours = Math.Abs(details.StartTime - 17.0);
-                double OverTimelDifferenceHours = Math.Abs(17.0 - details.EndTime);
-                NormalPay = NormalDifferenceHours * details.HourlyRate;
-                AdditionalPay = OverTimelDifferenceHours * details.HourlyRate * details.OvertimeMultiplier;
+                while (!(vowels.Contains(word[0])))
+                {
+                    word = word.Substring(1) + word[0];
+                    break;
+                }
+                word += "ay";
             }
-            else
+            
+            else if (re.IsMatch(word))
             {
-                double NormalDifferenceHours = Math.Abs(details.StartTime - details.EndTime);
-                NormalPay = NormalDifferenceHours * details.HourlyRate;
+                word += "yay";
             }
-            return NormalPay+Math.Round(AdditionalPay);
+            re = new Regex("[A-Z]");
+            if (re.IsMatch(word))
+            { 
+                word=word.ToLower();
+                word= char.ToUpper(word[0])+word.Substring(1);
+            }
+
+            return word;
         }
+        public static string TranslateSentence(string sentence)
+        {
+            Regex re = new Regex("[A-Za-z]+|[.!?\\-]+");
+            foreach (Match match in re.Matches(sentence))
+            { 
+                int loc=sentence.IndexOf(match.Value);
+                sentence=sentence.Remove(loc,match.Value.Length).Insert(loc, TranslateWord(match.Value));
+            }
+            return sentence;
+        }
+
         public static void Main(string[] args)
         {
 
-            Program SalaryCalculator = new Program();
-            Details details = new Details();
-            details.StartTime = 9;
-            details.EndTime = 17;
-            details.HourlyRate = 30;
-            details.OvertimeMultiplier = 1.5;
-            Console.WriteLine(SalaryCalculator.OverTime(details));
+            Console.WriteLine(TranslateSentence("I like to eat honey waffles."));
+            Console.WriteLine(TranslateSentence("Do you think it is going to rain today?"));
+
         }
     }
+    
 }
